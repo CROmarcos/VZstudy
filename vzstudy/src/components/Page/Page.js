d0c148033015752e2422aeb4b931b5d04c687e99
CROmarcos <markopavlovic2010@hotmail.com>
2022-01-26 02:41:45 +0100

    Prikaz na tabletu
    Na tabletu info kartice idu ispod slike.

--- vzstudy/src/components/Page/Page.js
+++ vzstudy/src/components/Page/Page.js
@@ -6,18 +6,30 @@ import "./Page.scss";
 const Page = (props) => {
   return (
     <div className="Page">
-      <div className="Page__Side">
+      <div className="Page__Side Desktop">
         <TextCard title="Studiji" smjerovi={props.smjerovi} />
       </div>
       <div className="Page__Center">
         <MainImage image={props.image} alt={props.alt} />
       </div>
-      <div className="Page__Side">
+      <div className="Page__Side Desktop">
         <InfoCard
           web={props.web}
           referada={props.referada}
           adresa={props.adresa}
         />
+      </div>
+      <div className="Tablet">
+        <div className="Page__Side">
+          <TextCard title="Studiji" smjerovi={props.smjerovi} />
+        </div>
+        <div className="Page__Side">
+          <InfoCard
+            web={props.web}
+            referada={props.referada}
+            adresa={props.adresa}
+          />
+        </div>
       </div>
     </div>
   );
